712157ce234fd36882b1a42aa326eeee248ffa8c
kasama <ka.sa.ma.123@gmail.com>
2025-03-30 13:20:18 +0900

    add menu example

---
+++ react/example/menu-app1/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div></div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
